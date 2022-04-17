21cea2ab1f8e091ac86cbdf854638480510e8eef
marsantiagope <marsantiagope@gmail.com>
2022-04-17 12:20:11 -0500

    First commit - Init settup of the software

---
+++ dashboard/src/main/java/com/aduma/dashboard/AdumaDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.aduma.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdumaDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdumaDashboardApplication.class, args);
+	}
+
+}
